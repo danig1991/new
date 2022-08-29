a1bc5bec56a3ed43eefbaff61025b30d7d07f416
Danil <danig1991@mail.ru>
2022-08-29 19:59:04 +0300

    hello

---
+++ Main.kt
@@ -0,0 +1,19 @@
+package dom02
+
+fun main() {
+    println("Hello!")
+    //Здравствуйте!
+    println("My name is _.")
+    //Меня зовут _.
+    println("I am from the _.")
+    //Я из _.
+    print("This is my first experience in IT. ")
+    print("I've never worked in this field before. ")
+    print("I want to learn how to program. ")
+    print("I think I should be able to do it. ")
+    /*Это мой первый опыт в IT.
+    * Раньше я никогда не работал в этой области.
+    * Хочу научиться программировать.
+    * Думаю, что у меня должно получиться.*/
+    //print("проигнорированная (закомментированная) строка")
+}
